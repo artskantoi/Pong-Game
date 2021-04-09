5337c9c1d3187ef92089457b87a65960cd586b4f
artskantoi <82163159+artskantoi@users.noreply.github.com>
2021-04-09 10:48:46 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,138 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":["81a4f500-ad11-4009-8f0e-175b165af74b","11d5d9f0-6932-45de-ba25-6f4ac37fe9a9","7d58941f-a7d8-42da-85c6-3efa6131461a","42b7aa29-c5f8-44fe-9d49-8540deca9c13","e92d7a76-cb9d-426f-90f2-1f5356f539aa"],"propsByKey":{"81a4f500-ad11-4009-8f0e-175b165af74b":{"name":"ball","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/81a4f500-ad11-4009-8f0e-175b165af74b.png","frameSize":{"x":20,"y":20},"frameCount":1,"looping":true,"frameDelay":4,"version":"yMBLRX0IBNNjnV4W4F8OEtmbzBSLDjS7","loadedFromSource":true,"saved":true,"sourceSize":{"x":20,"y":20},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/81a4f500-ad11-4009-8f0e-175b165af74b.png"},"11d5d9f0-6932-45de-ba25-6f4ac37fe9a9":{"name":"robot","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/11d5d9f0-6932-45de-ba25-6f4ac37fe9a9.png","frameSize":{"x":77,"y":69},"frameCount":1,"looping":true,"frameDelay":4,"version":"UO9NoWZL88290mMCTg8JkW4OT65VTRf4","loadedFromSource":true,"saved":true,"sourceSize":{"x":77,"y":69},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/11d5d9f0-6932-45de-ba25-6f4ac37fe9a9.png"},"7d58941f-a7d8-42da-85c6-3efa6131461a":{"name":"player","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/7d58941f-a7d8-42da-85c6-3efa6131461a.png","frameSize":{"x":60,"y":91},"frameCount":1,"looping":true,"frameDelay":4,"version":"Bb9GtmejmGUc.TVeRATxa8gq8cMhV3xp","loadedFromSource":true,"saved":true,"sourceSize":{"x":60,"y":91},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/7d58941f-a7d8-42da-85c6-3efa6131461a.png"},"42b7aa29-c5f8-44fe-9d49-8540deca9c13":{"name":"player_kick","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/42b7aa29-c5f8-44fe-9d49-8540deca9c13.png","frameSize":{"x":77,"y":77},"frameCount":1,"looping":true,"frameDelay":4,"version":"ErWWbjBvmBD2TcEF.HIoMW7Plo31WjSK","loadedFromSource":true,"saved":true,"sourceSize":{"x":77,"y":77},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/42b7aa29-c5f8-44fe-9d49-8540deca9c13.png"},"e92d7a76-cb9d-426f-90f2-1f5356f539aa":{"name":"player_fall","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/e92d7a76-cb9d-426f-90f2-1f5356f539aa.png","frameSize":{"x":92,"y":51},"frameCount":1,"looping":true,"frameDelay":4,"version":"pUgcG0OuPX2LWJDcS5JBViLkLF00pTAj","loadedFromSource":true,"saved":true,"sourceSize":{"x":92,"y":51},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/e92d7a76-cb9d-426f-90f2-1f5356f539aa.png"}}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+var ball = createSprite(200,200,10,10);
+ball.setAnimation("ball");
+var playerPaddle = createSprite(375,200,10,70);
+playerPaddle.setAnimation("player");
+var computerPaddle = createSprite(35,200,10,70);
+computerPaddle.setAnimation("robot");
+
+var gameState  = "serve";
+
+var playerScore = 0;
+var compScore = 0;
+
+function draw() {
+  background("blue");
+  
+ if (gameState === "serve"){
+    text("Press Space to Serve",150,180);
+  }
+  text(playerScore,220,20);
+  text(compScore,180,20);
+  
+  playerPaddle.y = World.mouseY;
+  
+  computerPaddle.y = ball.y;
+  
+  for (var i = 0; i < 400; i=i+20) {
+    line(200,i,200,i+10);
+  }
+  
+ 
+  
+ 
+  
+  createEdgeSprites();
+  
+  ball.bounceOff(topEdge);
+  ball.bounceOff(bottomEdge);
+  ball.bounceOff(playerPaddle);
+  ball.bounceOff(computerPaddle);
+ 
+  
+  //serve the ball when space is pressed
+  if (keyDown("space") && gameState === "serve" ) {
+    serve();
+    gameState = "play";
+  }
+  
+ 
+  //reset the ball to the centre if it crosses the screen
+  if(ball.x > 400 || ball.x <0) {
+    if(ball.x<0){
+      playerScore = playerScore + 1;
+    }
+    if(ball.x>400){
+      compScore = compScore + 1;
+    }
+    reset();
+    gameState = "serve";
+  }
+  if (playerScore === 5 || compScore === 5) {
+    gameState = "Over";
+    text("press a to restart", 150, 180);
+  }
+  if (keyDown("a")) {
+    gameState = "serve";
+    playerScore = 0;
+    compScore = 0;
+  }
+  
+  drawSprites();
+}
+
+function serve() {
+  ball.velocityX = 3;
+  ball.velocityY = 4;
+}
+
+function reset() {
+  ball.x = 200;
+  ball.y = 200;
+  ball.velocityX = 0;
+  ball.velocityY = 0;
+}
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
